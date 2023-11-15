cfd656296d1a1a8080e1e59b07cc6c05e26d93bd
Alej0oo <alejofocco09@gmail.com>
2023-11-15 02:04:44 -0300

    Editar paquetes funcional

--- js/gerente.js
+++ js/gerente.js
@@ -19,9 +19,13 @@ function buscarPaquete(ID_Cliente){
                     <td>${paquete.Descripcion}</td>
                     <td>${paquete.Peso_Kg}</td>
                     <td>${paquete.ID_Estado}</td>
-                    <td>${paquete.Destino}</td>`;
+                    <td>${paquete.Destino}</td>
+                    <td><button class="btn-a" onclick="datosPaquete('${paquete.ID}', '${paquete.ID_Cliente}', '${paquete.Descripcion}', '${paquete.Peso_Kg}', '${paquete.ID_Estado}', '${paquete.Destino}')" style="color: black;">Editar</button></td>
+                    <td><button class="btn-a" onclick="datosPaquete(${paquete.ID_Paquete})">Eliminar</button></td>
+                    `;
                 document.getElementById('tablaResultados').appendChild(resultado);
             });
+            document.getElementById("footer").style.position = "relative";
         },
         error: function(){
             alert("No se pudo encontrar el paquete");
@@ -103,4 +107,56 @@ function crearPaquete(ID_Gerente, Descripcion, Peso_Kg, ID_Cliente, Calle, Numer
     });
 }
 
+function datosPaquete(ID, ID_Cliente, Descripcion, Peso_Kg, ID_Estado, Destino) {
+    var partesDestino = Destino.split(',');
+    var Calle = partesDestino[0];
+    var Numero_Puerta = partesDestino[1];
+    var Ciudad = partesDestino[2];
 
+    window.location.href = `../paquete/editar.html?ID=${ID}&ID_Cliente=${ID_Cliente}&Descripcion=${Descripcion}&Peso_Kg=${Peso_Kg}&ID_Estado=${ID_Estado}&Calle=${Calle}&Numero_Puerta=${Numero_Puerta}&Ciudad=${Ciudad}`;
+}
+
+function editarPaquete(ID, ID_Cliente, Descripcion, Peso_Kg, ID_Estado, Calle, Numero_Puerta, Ciudad){
+    var ID = document.getElementById("ID").value;
+    var ID_Cliente = document.getElementById("ID_Cliente").value;
+    var Descripcion = document.getElementById("Descripcion").value;
+    var Peso_Kg = document.getElementById("Peso_Kg").value;
+    var ID_Estado = document.getElementById("ID_Estado").value;
+    var Calle = document.getElementById("Calle").value;
+    var Numero_Puerta = document.getElementById("Numero_Puerta").value;
+    var Ciudad = document.getElementById("Ciudad").value;
+
+    console.log(ID, ID_Cliente, Descripcion, Peso_Kg, ID_Estado, Calle, Numero_Puerta, Ciudad)
+
+    if (!ID_Cliente || !Descripcion || !Peso_Kg || !ID_Estado || !Calle || !Numero_Puerta || !Ciudad) {
+        alert("Completa todos los campos obligatorios en el formulario.");
+        return;
+    }
+
+    if (ID_Cliente === undefined || Descripcion === undefined || Peso_Kg === undefined || ID_Estado === undefined || Calle === undefined || Numero_Puerta === undefined || Ciudad === undefined) {
+        alert("Alguno de los valores es undefined. Verifica tu formulario.");
+        return;
+    }
+
+    jQuery.ajax({
+        url: "http://localhost:8001/api/gerente/paquetes",
+        type: "PATCH",
+        data: {
+            'ID': ID,
+            'Descripcion': Descripcion,
+            'ID_Cliente': ID_Cliente,
+            'Peso_Kg': Peso_Kg,
+            'ID_Estado': ID_Estado,
+            'Calle': Calle,
+            'Numero_Puerta': Numero_Puerta,
+            'Ciudad': Ciudad,
+        },
+        success: function (data) {
+            alert("Paquete actualizado con éxito");
+            window.location.href = "../paquete/buscar.html";
+        },
+        error: function () {
+            alert("No se pudo actualizar el paquete");
+        }
+    });
+}
